bf8e7410b5f79d6e5a3dab76e58daab7f4798851
zhuhua <zhuhua@macbook-air.local>
2016-04-18 09:38:06 +0800

    Initial Commit

---
+++ AutolayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutolayoutDemo
+//
+//  Created by kimi on 16/4/18.
+//  Copyright © 2016年 mooeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
